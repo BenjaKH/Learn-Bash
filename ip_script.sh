e2bf3e944f83fb745e81f49113400e537ae363bd
benjakh <benja.k.h@gmail.com>
2022-08-16 19:50:00 -0600

    added two lecture exersises

---
+++ ip_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Author: Benja K h
+#Date Created: 08/16/22
+#Last Modified:
+
+#Description
+# 
+
+#Usage: Information
+
+curl ifconfig.co > ~/ip.txt
---
+++ remote.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Author: Benja K h
+#Date Created: 08/16/22
+#Last Modified:
+
+#Description
+# 
+
+#Usage: Information
+
+echo "I am very far away" >> remote.txt
